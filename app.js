8530336f5e9ea04f60467f9128a8869ff4113af1
Alexandru <donialexandru@gmail.com>
2024-12-16 17:57:53 +0200

    add footer and update the header

--- app.js
+++ app.js
@@ -1,8 +1,2 @@
 // It's better to wait for the event for manipulation
-window.addEventListener("DOMContentLoaded", () => {
-  let nav = document.querySelector("nav");
-  console.log(nav);
-  nav.innerHTML = `
-    <h2>This is the text changed from code</h2>
-`;
-});
+window.addEventListener("DOMContentLoaded", () => {});
