d071e0f4b60724c59f87779cc8bd056bf028bbdb
Alexandru <donialexandru@gmail.com>
2024-12-17 15:45:20 +0200

    implement Router service

---
+++ services/Router.js
@@ -0,0 +1,44 @@
+const Router = {
+  init: () => {
+    document.querySelectorAll("a.navlink").forEach((a) => {
+      a.addEventListener("click", (event) => {
+        event.preventDefault();
+        const url = event.target.getAttribute("href");
+        Router.go(url);
+      });
+    });
+    // Event Handler for URL changes
+    window.addEventListener("popstate", (event) => {
+      Router.go(event.state.route, false);
+    });
+
+    // Check the initial URL
+    Router.go(location.pathname);
+  },
+  go: (route, addToHistory = true) => {
+    if (addToHistory) {
+      history.pushState({ route }, "", route);
+    }
+    let pageElement = null;
+    switch (route) {
+      case "/":
+        pageElement = document.createElement("h1");
+        pageElement.textContent = "This is home page";
+        break;
+      case "/projects":
+        pageElement = document.createElement("h1");
+        pageElement.textContent = "This is the projects page";
+        break;
+    }
+
+    if (pageElement) {
+      // document.querySelector("main").childred[0].remove();
+      const cache = document.querySelector("main");
+      cache.innerHTML = "";
+      cache.appendChild(pageElement);
+      window.scrollX = 0;
+      window.scrollY = 0;
+    }
+  },
+};
+export default Router;
