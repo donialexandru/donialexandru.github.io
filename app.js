c28de68568d98aebf7fac0bb63c4a3b133f3e882
Alexandru <donialexandru@gmail.com>
2024-12-17 15:45:01 +0200

    add Router to the app

--- app.js
+++ app.js
@@ -1,2 +1,9 @@
+import Router from "./services/Router.js";
+
 // It's better to wait for the event for manipulation
-window.addEventListener("DOMContentLoaded", () => {});
+window.app = {};
+app.router = Router;
+
+window.addEventListener("DOMContentLoaded", () => {
+  app.router.init();
+});
